34015541a3255d617acd46b1a45084ff71a6df64
OisinFoley <oisin.foley@jayway.com>
2023-02-10 22:55:57 +0100

    added OrderEntry, ScoopOption + TooppingOptions components (among others), along with axios package + updated react test npm script

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import OrderEntry from "./pages/entry/OrderEntry";
 import SummaryForm from "./pages/summary/SummaryForm";
 
 function App() {
   return (
     <div className="App">
-      <SummaryForm />
+      {/* <SummaryForm /> */}
+      <OrderEntry />
     </div>
   );
 }
